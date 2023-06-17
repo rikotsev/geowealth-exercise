cde46375b6b144b2a7c69ea6dea2b755972c9156
rkotsev <radoslav.kotsev@factset.com>
2023-06-17 08:36:46 +0300

    Disabled validation

--- src/main/java/org/exercise/Main.java
+++ src/main/java/org/exercise/Main.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    private static final boolean ENABLE_VALIDATION = true;
+    private static final boolean ENABLE_VALIDATION = false;
 
     public static void main(String[] args) {
         try {
